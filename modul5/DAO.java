a8c41c252b751c05b915e33c06e34901dd01328b
admin <anton.words@gmail.com>
2017-01-15 17:53:27 +0200

    Comm

---
+++ modul5/DAO.java
@@ -0,0 +1,8 @@
+package modul5;
+
+public interface DAO {
+    Room save(Room room);
+    boolean delete(Room room);
+    Room update(Room room);
+    Room findById(long id);
+}
---
+++ test/Main.java
@@ -0,0 +1,22 @@
+package test;
+
+/**
+ * Created by admin on 015 15.01.17.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        String[] a = new String[2];
+        a[0] = "4";
+        a[1] = String.valueOf((Integer) 2);
+
+
+
+        a[0]= "stop rre";
+//        a[1]= 't';
+//        a[2]= 'o';
+//        a[3]= 'p';
+        System.out.println(a[1]+3);
+    }
+}
