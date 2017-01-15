4d3e7c3489a62de3bf4ca5a1d7495d3e3d375a37
admin <anton.words@gmail.com>
2017-01-15 18:33:33 +0200

    Comm

--- modul5/DAOImpl.java
+++ modul5/DAOImpl.java
@@ -1,26 +1,33 @@
 package modul5;
 
+import java.util.Date;
+
 /**
  * Created by admin on 015 15.01.17.
  */
 public class DAOImpl implements DAO {
     @Override
     public Room save(Room room) {
-        return null;
+        System.out.println("Комната " +room.getId() + " сохранена.");
+        return room;
     }
 
     @Override
     public boolean delete(Room room) {
-        return false;
+        System.out.println("Комната " +room.getId() + " удалена.");
+        return true;
     }
 
     @Override
     public Room update(Room room) {
-        return null;
+        System.out.println("Комната " +room.getId() + " обновленна.");
+        return room;
     }
 
     @Override
     public Room findById(long id) {
-        return null;
+        Room room = new Room(20, 300, 2,new Date(),"hotel", "city"); // эту часть я подсмотрел. Не пойму почему так происходит? для чего нужно создавть экземпляр?
+        System.out.println("Комната с ID " +id + " найдена.");
+        return room;
     }
 }
